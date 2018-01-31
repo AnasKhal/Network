1fbe957c91e0a59b8cd3cec68820a60db5bc106c
anas-khal <anas-khal@users.noreply.github.com>
2018-01-31 11:47:09 +0000

    20180131T114649Z

--- mario.c
+++ mario.c
@@ -10,34 +10,19 @@ int main(void)
   }
 
   while (n < 0 || n > 23);
-//print evry line
-   for (int l = 0; l < n;l++)
-    {
-        //print spaces for left
-        int i = 0;
-        while(i < n-l-1)
-        {
-            printf(" ");
-            i++;
-        }
 
-        //on left
-        int j = 0;
-        while(j < l+1)
-        {
-            printf("#");
-            j++;
-        }
-        //2 spaces
-        printf("  ");
-        //on right
-        int h = 0;
-        while(h < l+1)
-        {
-            printf("#");
-            h++;
-        }
-        // new line
-        printf("\n");
-    }
+  for (int i = 0; i < n; i++)
+    {
+        //for spaces
+          for (int j = 0; j < n-i-1 ; j++ )
+          {
+          printf(" ");
+
+          }
+for (int j = 0; j < i + 2 ; j++ )
+{
+    printf("#");
+}
+printf("\n");
+}
 }
