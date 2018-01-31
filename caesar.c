1c885f0edc6e6b547d8001c21d8ebf9930d750d6
anas-khal <anas-khal@users.noreply.github.com>
2018-01-31 13:57:27 +0000

    20180131T135710Z

--- caesar.c
+++ caesar.c
@@ -1,51 +1,54 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 int main(int argc, string argv[])
-{
-    bool keySuccessful = false;
-    int key = 0;
-    int input_length = 0;
-    string text = "";
 
-    if(argc != 2)
-        {
-            printf("Please provide a command line argument.\n");
-            return 1;
-    }
-else
 {
-    key = atoi(argv[1]);
-    keycorrect = true;
-}
-    } while(!keycorrect);
 
-    text = GetString();
-    input_length = strlen(text);
-    for(int i = 0; i < input_length; i++)
+    if (argc != 2 || argv[1] < 0)
+
     {
-        if(isalpha(text[i]))
+        printf("Wrong please try again!.\n");
+        return 1;
+    }
+
+
+    int k = atoi(argv[1]);
+
+    if (k < 1)
+    {
+        printf("A positive interger Please.\n");
+        return 1;
+    }
+    else
+    {
+
+        k %= 26;
+    }
+
+    string text = GetString();
+
+    char c, start;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        c = text[i];
+
+        if (isalpha(c))
         {
-
-             if(islower(text[i]))
-            {
-                printf("%c", ((((text[i] - 97)+key)%26)+97));
-            }
-
-            else
-            {
-                printf("%c", ((((text[i] - 65)+key)%26)+65));
-            }
+            start = isupper (c) ? 'A' : 'a';
+            printf("%c", start + (c - start + k)%26);
         }
         else
-        {
-            printf("%c", text[i]);
-        }
+            printf("%c", c);
     }
+
 
     printf("\n");
     return 0;
+}
 }
