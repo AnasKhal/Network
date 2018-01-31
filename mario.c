aeba0b901f917acbed77e8924cb770ebe58a6753
anas-khal <anas-khal@users.noreply.github.com>
2018-01-31 11:24:34 +0000

    20180131T112416Z

--- mario.c
+++ mario.c
@@ -3,11 +3,25 @@
 
 int main(void)
 {
-    int n = get_int("height: ");
+    int n;
+    do
+  {
+      n = get_int("height: ");
+  }
+
+  while (n < 0 || n > 23);
 
   for (int i = 0; i < n; i++)
+    {
+          for (int j = 0; j < n - 1 - i + 1; j++ )
+          {
+          printf(" ");
 
-
-    printf("#\n");
-
+          }
+for (int j = 0; j < i + 2 ; j++ )
+{
+    printf("#");
+}
+printf("\n");
+}
 }
