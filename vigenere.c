dcdf303233ef62782b370f41b332aa95f8017b9c
anas-khal <anas-khal@users.noreply.github.com>
2018-01-31 14:45:05 +0000

    20180131T144447Z

--- vigenere.c
+++ vigenere.c
@@ -8,61 +8,69 @@ int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./vigenere k\n");
+        printf("INVALID\n");
         return 1;
     }
 
-    string key = argv[1];
-    for(int i= 0; i < strlen(key); i++)
+    string k = argv[1];
+
+    //check for whether all alphabetical keys
+
+    for(int i= 0; i < strlen(k); i++)
     {
-      if(!isalpha(key[i]))
+      if(!isalpha(k[i]))
       {
-          printf("Theres an error\n");
-          return 1;
+          printf("Not all alphabetical\n");
+          return -1;
       }
     }
+      string plain = GetString();
+      int counter = 0;
+      char c = 'a';
+      int goo = 0; //for doing ascii math in loop later
+      for (int r=0; r<strlen(plain); r++){
 
-    int keyLen = strlen(key);
-
-    printf("plaintext: ");
-
-    string p = get_string();
-
-    printf("ciphertext: ");
-
-    for(int i = 0,j = 0; i < strlen(p); i++)
-    {
-      if(j == keyLen)
-      {
-        j = 0;
-      }
-
-      int keyCon;
-
-      if(isupper(key[j]))
-      {
-        keyCon = key[j] - 65;
-      }
-      else
-      {
-        keyCon = key[j] - 97;
-      }
-
-
-        if(isupper(p[i]))
-        {
-          printf("%c", ((((p[i] + (keyCon)) - 65) % 26) + 65));
-        }
-        else if(islower(p[i]))
-        {
-          printf("%c", ((((p[i] + (keyCon)) - 97) % 26) + 97));
-        }
-        else{
-          printf("%c", p[i]);
-        }
-
-        j++;
+    if (!isalpha(plain[r])){
+        counter++;
+        printf("%c", plain[r]); //print element if its a not a letter
     }
-          printf("\n");
-          return 0;
+    else{
+        //initialize
+        int z = plain[r]; //convert to number
+        int t = ((r - counter)%strlen(k)); //helps cycle through smaller passcode, remembers spot
+        int y = k[t]; //T is for placeholder of key code relative to R for plaintext
+
+
+        if (isupper(plain[r])){
+            if(isupper(k[t])){
+                goo = ((((z-65)+(y-65))%26)+65); //goes by plain value
+                c = goo;
+                printf("%c", c);
+
+            }
+            else{
+                 goo = ((((z-65)+(y-97))%26)+65);
+                 c = goo;
+                printf("%c", c);
+            }
+
+
+        }
+        if (islower(plain[r])){
+            if(isupper(k[t])){
+                goo = ((((z-97)+(y-65))%26)+97);
+                c = goo;
+                printf("%c", c);
+
+            }
+            else{
+                goo = ((((z-97)+(y-97))%26)+97);
+                c = goo;
+                printf("%c", c);
+            }
+        }
+}
+}
+printf("\n");
+return 0;
 }
